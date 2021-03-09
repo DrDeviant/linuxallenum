5c794f69408d316e4bb6a1d11a280aac388dc846
FabioDefilippo <71832873+FabioDefilippo@users.noreply.github.com>
2021-03-09 10:08:44 +0100

    Update linuxallenum.sh

--- linuxallenum.sh
+++ linuxallenum.sh
@@ -1139,7 +1139,7 @@ while true; do
 					read -p "(example, 1337): " PORT
 					if [[ "$PORT" != "" ]];
 					then
-						ssh -L "$LPORT"":""$IP"":"$PORT"
+						ssh -L "$LPORT"":""$IP"":""$PORT"
 					fi
 				fi
 			fi
